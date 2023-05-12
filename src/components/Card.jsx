181f6ba55747831b0c7d447fc099a377dbf99a8b
Suhendar70100 <suhendarteguh0@gmail.com>
2023-05-12 13:01:48 +0700

    add confoguration vercel

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -109,7 +109,7 @@ export default function Card({ rooms }) {
                     <div className="modal-dialog">
                         <div className="modal-content">
                             <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                                <h1 className="modal-title fs-5 text-center" id="staticBackdropLabel">Update Room</h1>
                                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div className="modal-body">
